27f3570d9dd88e001a0a982a3a661d0fb1c1998e
nalcsmn <nlacsamana09@gmail.com>
2022-03-29 22:44:50 +0800

    first commit

---
+++ src/pages/Help.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique quidem laborum nobis accusamus nostrum molestias, vel dolore, unde praesentium atque cum maxime quod tempore repudiandae. Consequuntur eos adipisci iste explicabo?</p>
+   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique quidem laborum nobis accusamus nostrum molestias, vel dolore, unde praesentium atque cum maxime quod tempore repudiandae. Consequuntur eos adipisci iste explicabo?</p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+ 
+})
+</script>
